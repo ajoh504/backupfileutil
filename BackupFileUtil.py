99357dfbf92f2984c63984eddfd68028a71f63f1
Alex <83892813+ajoh504@users.noreply.github.com>
2022-04-23 09:17:38 -0700

    Update BackupFileUtil.py
    Added welcome message and user options
    Added print statements

--- BackupFileUtil.py
+++ BackupFileUtil.py
@@ -5,44 +5,47 @@ from pathlib import Path
 import os
 import shelve
 
+
 def main():
+    # TODO: prompt for user input from user options table
+    input()
     # Retrieve user home directory, check if BackupFileUtil dir exists
-    newDir = Path(Path.home()) / 'BackupFileUtil'
-    print(newDir)
+    newDir = Path(Path.home()) / "BackupFileUtil"
     if newDir.exists() == False:
         os.makedirs(newDir)
 
-    # TODO: Create .txt file for storing file paths
-
-    os.chdir(Path(Path.home()) / 'BackupFileUtil')
-    print(Path.cwd())
-    storedPaths = open('storedPaths.txt', 'w')
-
-        # TODO: prompt for user input from user options table
-
-    # TODO: Create table for user options:
-
-        # First time setup?
-
-        # Select backup drive and source drive
-
-        # Add new file path?
-
-        # View existing filepaths?
-
-        # Configuration (change default .txt location) 
-
+    # Create .txt file for storing file paths
+    os.chdir(Path(Path.home()) / "BackupFileUtil")
+    storedPaths = open("storedPaths.txt", "w")
 
     # TODO: Execute commands from user input
 
-        # optionally print existing file paths
+    # optionally print existing file paths
 
-        # optionally write to .txt file based on user input
+    # optionally write to .txt file based on user input
 
-        # optionally backup files
+    # optionally backup files
 
-        # optionally display existing file paths or more info
+    # optionally display existing file paths or more info
+
 
 if __name__ == "__main__":
-    main()
+    WELCOME_MESSAGE = " Welcome to BackupFileUtil v. 1.0 "
+    WELCOME_PARA = """
+\nUse this script to store and manage files and directories that you
+wish to save to an external drive. If this is a first time setup,
+please select 1 for easy setup or 2 to configure the default script
+file locations.\n"""
+    USER_OPTIONS = {
+        1: "First time setup: easy",
+        2: "First time setup: advanced",
+        3: "Add new file path",
+        4: "View existing filepaths",
+        5: "About",
+    }
 
+    print(WELCOME_MESSAGE.center(68, "-"), WELCOME_PARA)
+    for number, message in USER_OPTIONS.items():
+        print(message.ljust(30, ".") + str(number).rjust(2))
+    while True:    
+        main(input())
