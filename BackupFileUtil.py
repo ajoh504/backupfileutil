e4e4f295f173ce6ac6b2e78305c560ee38342e4f
Alex <83892813+ajoh504@users.noreply.github.com>
2022-06-11 10:20:24 -0700

    Update BackupFileUtil.py
    1. moved try/except from return_drives() to main()
    
    2. edited return_drives() to contain list comprehension 
    
    3. edited messages for clarity 
    
    4. edited variable names for clarity

--- BackupFileUtil.py
+++ BackupFileUtil.py
@@ -10,44 +10,42 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -  %(message)s')
 logging.debug("program start")
 
-class mainDirConfig: # Add and/or manage main program directory
+class mainDirConfig:  # Add and/or manage main program directory
     def __init__(self, new_path):
         self.new_path = new_path
 
-    def add_main_dir(self) -> None: # add "BackupFileUtil" to the User's home directory
+    def add_main_dir(self) -> None:  # add "BackupFileUtil" to the User's home directory
         main_dir_config = Path(Path.home()) / "BackupFileUtil"
         if main_dir_config.exists() == False:
             main_dir_config.mkdir()
 
-    def edit_text_file(self) -> None: # edit text file containing file paths
+    def edit_text_file(self) -> None:  # edit text file containing file paths
         stored_paths = open(Path.home() / "BackupFileUtil/storedPaths.txt", "a")
         stored_paths.write(self.new_path + "\n")
         stored_paths.close()
 
+
 class sourceConfig:
     def __init__(self):
         pass
+
     # TODO: return joined list with spaces and numbered elements for each drive
     @staticmethod
-    def return_drives() -> list or str: # return a list of all available storage media
+    def return_drives() -> str:  # return all available storage media
         DRIVE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        try:
-            return list(
-                letter + ":/" for letter in DRIVE_LETTERS if Path(letter + ":/").exists()
-            )
-        except OSError:
-            return OS_ERROR_MESSAGE
+        return ' '.join([letter + ":/" for letter in DRIVE_LETTERS if Path(letter + ":/").exists()])
 
     @staticmethod
-    def get_paths() -> list: # return a list of all stored file paths
+    def get_paths() -> list:  # return a list of all stored file paths
         stored_paths = open(Path.home() / "BackupFileUtil/storedPaths.txt", "r")
         path_list = stored_paths.readlines()
         stored_paths.close()
         return path_list
 
+
 class backupConfig:
     def __init__(self, destination: str, source_list: list) -> None:
-        self.destination = destination + 'Backups/' # dir named 'Backups' on destination drive
+        self.destination = destination + 'Backups/'  # dir named 'Backups' on destination drive
         self.source_list = source_list
 
     def copy_tree(self, dir_path: str) -> None:
@@ -70,72 +68,71 @@ class backupConfig:
             elif Path(file_path).is_file():
                 self.copy_files(file_path)
 
+
 def main() -> None:
-    print("\n", WELCOME_MESSAGE.center(68, "-"), WELCOME_PARA) # print startup messages
-    while True: # main selection loop
-        for number, message in MAIN_MENU.items(): # print main menu
+    print("\n", WELCOME_MESSAGE.center(82, "-"), WELCOME_PARA)  # print startup messages
+    while True:  # main selection loop
+        for number, message in MAIN_MENU.items():  # print main menu
             print(message.ljust(30, ".") + str(number).rjust(2))
-        choice = input()
-        if choice not in "12345":
+        main_menu_choice = input()
+        if main_menu_choice not in "12345":
             print("Invalid selection\n")
             continue
-        match choice:
-            case '1': # First time setup
+        match main_menu_choice:
+            case '1':  # First time setup
                 print(SETUP_MESSAGE)
                 while True:
-                    path_or_break = input(NEW_PATH_MESSAGE)
-                    main_dir_config = mainDirConfig(path_or_break)
+                    case_one_choice = input(NEW_PATH_MESSAGE)
+                    main_dir_config = mainDirConfig(case_one_choice)
                     main_dir_config.add_main_dir()
-                    if Path(path_or_break).exists():
+                    if Path(case_one_choice).exists():
                         main_dir_config.edit_text_file()
-                    elif path_or_break.lower() == 'b':
+                    elif case_one_choice.lower() == 'b':
                         break
                     else:
                         continue
 
-            case '2': # Add new file path
+            case '2':  # Add new file path
                 while True:
-                    path_or_break = input(NEW_PATH_MESSAGE)
-                    main_dir_config = mainDirConfig(path_or_break)
+                    case_two_choice = input(NEW_PATH_MESSAGE)
+                    main_dir_config = mainDirConfig(case_two_choice)
                     main_dir_config.add_main_dir()
-                    if Path(path_or_break).exists():
+                    if Path(case_two_choice).exists():
                         main_dir_config.edit_text_file()
-                    elif path_or_break.lower() == 'b':
+                    elif case_two_choice.lower() == 'b':
                         break
                     else:
                         continue
 
-            case '3': # View existing file paths
+            case '3':  # View existing file paths
                 for path in sourceConfig.get_paths():
                     print(path)
-                print('\n')
 
-            case '4': # Run Backup
+            case '4':  # Run Backup
                 while True:
-                    if type(sourceConfig.return_drives()) == str:
-                        print(sourceConfig.return_drives())
-                        break
-                    elif type(sourceConfig.return_drives()) == list:
+                    try:
                         print(BACKUP_MESSAGE, sourceConfig.return_drives(), BACKUP_WARNING)
-                        destination_path = input()
-                        if destination_path.lower() == 'b':
+                        case_four_choice = input()
+                        if case_four_choice.lower() == 'b':
                             break
-                        elif Path(destination_path).exists() == True:
-                            backup_config = backupConfig(destination_path, sourceConfig.get_paths())
+                        elif Path(case_four_choice).exists() == True:
+                            backup_config = backupConfig(case_four_choice, sourceConfig.get_paths())
                             backup_config.back_up_files()
                             break
-                        elif Path(destination_path).exists() == False:
+                        elif Path(case_four_choice).exists() == False:
                             print(INVALID_BACKUP_PATH)
                             continue
+                    except OSError:
+                        print(sourceConfig.return_drives(), OS_ERROR_MESSAGE)
 
-            case '5': # EXIT
+            case '5':  # EXIT
                 exit()
-                
+
+
 WELCOME_MESSAGE = " BackupFileUtil "
 WELCOME_PARA = """
-\nUse this script to store and manage files and directories that you
-wish to save to an external drive. If this is a first time setup,
-please select 1.\n"""
+\nUse this script to store and manage files and directories that you wish to save to 
+an external drive. If this is a first time setup, please select 1.\n"""
 MAIN_MENU = {
     1: "First time setup",
     2: "Add new file path",
@@ -143,24 +140,19 @@ MAIN_MENU = {
     4: "Run Backup",
     5: "EXIT",
 }
-SETUP_MESSAGE = """Please enter any file paths you wish to store for
-future backups. If path leads to a directory, all files and
-subdirectories will be copied to the backup drive. If path
-leads to a file, the file will be copied and the parent directory
-will be created on the backup drive.\n"""
-NEW_PATH_MESSAGE = 'Please enter path, or type "B" to return to main menu.\n'
-BACKUP_MESSAGE = (
-    "Please enter a path to store your backups. The following drives are available:\n"
-)
-BACKUP_WARNING = """\n\nWARNING: if you proceed, any files and directories on the backup drive
-with the same name as the source drive will be completely overwritten. This cannot be undone!
-Type 'b' if you wish to return to the main menu, or enter a path to the backup drive to proceed."""
-INVALID_BACKUP_PATH = (
-    "Invalid destination path. Please enter a valid file path to store your backups.\n"
-)
-OS_ERROR_MESSAGE = '''\nWARNING. One of the discovered drives may be corrupt or may be formatted with an invalid
+SETUP_MESSAGE = """
+Please enter a path to any file you wish to store for future backups. Directory paths 
+will be copied with all files and subdirectories.\n"""
+NEW_PATH_MESSAGE = "Please enter a file path, or type 'B' to return to main menu.\n"
+BACKUP_MESSAGE = "\nPlease type in a drive location to store your backups. The following drives are available:\n"
+BACKUP_WARNING = """
+\nWARNING: if you proceed, any files and directories on the backup drive with the same 
+name as the source drive will be completely overwritten. This cannot be undone! Type 'B' 
+if you wish to return to the main menu, or type in a backup drive letter to proceed.\n"""
+INVALID_BACKUP_PATH = "Invalid destination path. Please enter a valid file path to store your backups."
+OS_ERROR_MESSAGE = '''
+\nWARNING. One of the discovered drives may be corrupt or may be formatted with an invalid
 drive type. Please check the volume type of your backup drive(s) before proceeding.\n'''
-
 
 if __name__ == "__main__":
     main()
