46c7d1a1ae04327a098574f3d2a36416b31eb2fc
Alex Johnson <83892813+ajoh504@users.noreply.github.com>
2022-09-25 08:12:01 -0700

    Update backupfileutil.py
    1. Re-added comments from prior version

--- backupfileutil.py
+++ backupfileutil.py
@@ -10,6 +10,7 @@ from datetime import datetime
 
 
 def copy_tree(destination: str, dir_path: str) -> None:
+    """Copy directory and all of its files and subdirectories to destination drive."""
     shutil.copytree(
         dir_path,
         destination + "/".join(Path(dir_path).parts[1:]),
@@ -18,10 +19,19 @@ def copy_tree(destination: str, dir_path: str) -> None:
 
 
 def copy_files(destination: str, file_path: str) -> None:
+    """Copy files to destination drive."""
     shutil.copy(file_path, destination + "/".join(Path(file_path).parts[1:-1]))
 
 
 def back_up_files(destination: str, file_list: str) -> None:
+    """
+    Run the main backup script by looping through the paths from storedpaths.txt. If
+    file_path is a directory, then copy all of its files and subdirectories to the
+    backup folder. If file_path is a file, check to make sure its parent directories
+    exist on the destination drive. If parents do not exist, create them on the
+    destination drive and copy the file. Lastly, copy any remaining files from the
+    source_list.
+    """
     for file_path in file_list:
         file_path = file_path.strip("\n")
         if Path(file_path).is_dir() == True:
@@ -37,25 +47,29 @@ def back_up_files(destination: str, file_list: str) -> None:
             copy_files(destination, file_path)
 
 
-def edit_text_file(new_path: str) -> None:  # edit text file containing file paths
+def edit_text_file(new_path: str) -> None:
+    """edit text file containing file paths"""
     with open(Path.home() / "backupfileutil/storedpaths.txt", "a") as stored_paths:
         stored_paths.write(new_path + "\n")
 
 
-def add_main_dir() -> None:  # add "backupfileutil" to the User's home directory
+def add_main_dir() -> None:
+    """add "backupfileutil" to the User's home directory"""
     main_dir = Path(Path.home()) / "backupfileutil"
     if main_dir.exists() == False:
         main_dir.mkdir()
 
 
-def return_drives() -> str:  # return all available storage media
+def return_drives() -> str:
+    """Return all available storage media"""
     DRIVE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     return " ".join(
         [letter + ":/" for letter in DRIVE_LETTERS if Path(letter + ":/").exists()]
     )
 
 
-def get_paths() -> list:  # return a list of all stored file paths
+def get_paths() -> list:
+    """Return a list of all stored file paths"""
     with open(Path.home() / "backupfileutil/storedpaths.txt", "r") as stored_paths:
         return stored_paths.readlines()
 
@@ -68,11 +82,11 @@ def get_json_data() -> dict:
 def main():
     add_main_dir()
     TEXT = get_json_data()
-    print(
-        "\n", TEXT["WELCOME"].center(82, "-"), "\n".join(TEXT["WELCOME_PARA"])
-    )  # print startup messages
 
-    while True:  # main selection loop
+    """Print startup messages"""
+    print("\n", TEXT["WELCOME"].center(82, "-"), "\n".join(TEXT["WELCOME_PARA"]))
+
+    while True:
         for number, message in TEXT["MAIN_MENU"].items():  # print main menu
             print(message.ljust(30, ".") + str(number).rjust(2))
         choice = input()
